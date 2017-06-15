81b502a3625373ce6e583ff20a0079245b52b0a5
[Chimezie Anyaoha] <[chimezie.anyaoha@gmail.com]>
2017-06-15 14:42:15 -0400

    added Navbar Component and imported to Voicepad.js

---
+++ my-voicepad/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className='navbar'>
+                <h2>My Voicepad</h2> 
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
--- my-voicepad/src/Components/Voicepad.js
+++ my-voicepad/src/Components/Voicepad.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import Navbar from './Navbar'
+// import {
+//     BrowserRouter as Router,
+//     Route,
+//     Link
+// } from 'react-router-dom';
 
 class Voicepad extends Component {
     render(){
         return (
-            <div>
+            <main>
+                <Navbar/>
                 <h1>chim</h1>
-            </div>
+                
+            </main>
         )
     }
 }
